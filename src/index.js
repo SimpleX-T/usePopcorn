4c7ee5268b931d3ad323e4df6ed5f4bdb8817cf6
Xcodes <ntmark2004@gmail.com>
2024-02-27 11:26:11 +0100

    ran build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
